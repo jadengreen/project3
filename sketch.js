7a6d89ebd45852973c36750fed363071f35a91af
jadengreen <55153277+jadengreen@users.noreply.github.com>
2019-10-17 09:25:57 -0700

    new code

--- sketch.js
+++ sketch.js
@@ -1,24 +1,26 @@
-function setup() {
-  createCanvas(640, 480);
+var img;
+function preload() {
+image = LoadImage("images/smileyface.jpg");
 }
+
+function setup () {
+
+  createCanvas(400, 400);
+
+}
+
+
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+
+  background(50,50,100);
+
+  rect (30,10,200,10)
+
+  rect (30,100,200,400)
+
+  fill (20,15,60)
+
+  image(img,mouseX,mouseY)
+
 }
-
-function setup() {
-createCanvas(480, 120);
-}
-
-function draw() {
-  background(204);
-  fill(250, 0, 0);
-  rect(180, 60, 220, 40);
- }
-
- 
